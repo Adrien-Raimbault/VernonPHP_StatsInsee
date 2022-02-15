0feeeb86e88f5b9409f79f90fa3fc05ba97e245a
Adrien RAIMBAULT <adrien-raimbault@protonmail.com>
2022-02-15 12:28:05 +0100

    création fonction createTab()
    Appel d'une fonction pour afficher le tableau

--- index.php
+++ index.php
@@ -2,39 +2,51 @@
 
 echo '<h1>Stats Insee</h1>';
 
+/**
+ * Affichage d'un tableau depuis un fichier .csv
+ *
+ * @param string $fichier path du fichier à ouvrir
+ * @param integer $ligneEntete ligne contenant l'en-tête du tableau
+ * @param integer $ligneDepart première ligne contenant les données à afficher
+ * @return void
+ */
+function createTab(string $fichier , int $ligneEntete, int $ligneDepart){
+    // OUVERTURE DU FICHIER SOUS FORME D'ARRAY
+    $fichier2 = file($fichier);
+
+    // OUVERTURE TABLEAU
+    $tableau = '<table style = "text-align:center">';
+    $tabHead = '<thead>';
+    $tabBody = '<tbody>';
+
+    // PARSING DU TABLEAU
+    for($i = 0; $i < count($fichier2); $i++){
+        $test = explode(';', str_replace([' - ', '"'], ['<br />',""],$fichier2[$i]));
+
+        if ($i == $ligneEntete){
+            $tabHead .='<tr>';
+            foreach($test as $cell){
+                $tabHead .='<th>' . $cell . '</th>';
+            }
+            $tabHead .='</tr>';
+        }
+        elseif ($i > $ligneDepart){
+            $tabBody .='<tr>';
+            foreach($test as $cell){
+                $tabBody .='<td>' . $cell . '</td>';
+            }
+            $tabBody .='</tr>';
+        }
+    }
+    // FERMETURE TABLEAU
+    $tabHead .= '</thead>';
+    $tabBody .= '</tbody>';
+    $tableau .= $tabHead . $tabBody .'</table>';
+
+    echo $tableau;
+}
+
 // CHEMIN ABSOLU DU FICHIER
 $path = './data/valeurs_mensuelles.csv';
 
-// OUVERTURE DU FICHIER SOUS FORME D'ARRAY
-$fichier2 = file($path);
-// var_dump($fichier2);
-
-// PARSING DU TABLEAU
-$tableau = '<table style = "text-align:center">';
-$tabHead = '<thead>';
-$tabBody = '<tbody>';
-
-for($i = 0; $i < count($fichier2); $i++){
-    $test = explode(';', str_replace([' - ', '"'], ['<br />',""],$fichier2[$i]));
-
-    if ($i == 0){
-        $tabHead .='<tr>';
-        foreach($test as $cell){
-            $tabHead .='<th>' . $cell . '</th>';
-        }
-        $tabHead .='</tr>';
-    }
-    elseif ($i > 4){
-        $tabBody .='<tr>';
-        foreach($test as $cell){
-            $tabBody .='<td>' . $cell . '</td>';
-        }
-        $tabBody .='</tr>';
-    }
-}
-
-$tabHead .= '</thead>';
-$tabBody .= '</tbody>';
-$tableau .= $tabHead . $tabBody .'</table>';
-
-echo $tableau;
+createTab($path, 0, 4);
